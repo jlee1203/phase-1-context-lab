f67350990e1cf7490a8cbfd5d1a7b2bdf56dece3
James Lee <j.lee1203@hotmail.com>
2022-09-24 10:53:35 -0700

    commit tree 271927790ded01b0f2081c72319cb6ca63df6cdc

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
 /* Your Code Here */
+function createEmployeeRecord(employee){
+   return {
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: []
+   }
+}
+
+function createEmployeeRecords(employees){
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(event){
+    const [date, hour] = event.split(" ")
+    const eventObject = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    }
+
+    this.timeInEvents.push(eventObject);
+    return this;
+}
+
+function createTimeOutEvent(event){
+    const [date, hour] = event.split(" ")
+    const eventObject = {
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    }
+
+    this.timeOutEvents.push(eventObject);
+    return this;
+}
+
+function hoursWorkedOnDate(date){
+    const inTime = this.timeInEvents.find(event => event.date === date);
+    const outTime = this.timeOutEvents.find(event => event.date === date);
+
+    return (outTime.hour - inTime.hour)/100
+}
+
+function wagesEarnedOnDate(date){
+    const hours = hoursWorkedOnDate.call(this, date);
+    return this.payPerHour * hours
+}
+
+function calculatePayroll(employeeRecords){
+    const record = employeeRecords.map(employee => allWagesFor.call(employee))
+    return record.reduce((currentValue, total) => currentValue + total)
+ }
+
+ function findEmployeeByFirstName(employees, firstNameString){
+    console.table(employees)
+    return employees.find(emp => emp.firstName === firstNameString)
+ }
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
